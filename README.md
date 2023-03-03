02820e69643d48dffa4bc5374b098e886f637637
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-03-03 15:56:41 +0400

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# magic_ball_flutter
+
+<br/>
+
+<div align="center">
+  <img src="ss.jpg" width="230px" />
+</div>
+
+<br/>
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+void main() {
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade300,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blue,
+        ),
+        body: ImagePage(),
+      ),
+    );
+  }
+}
+
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({Key? key}) : super(key: key);
+
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+
+  var number = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            number = Random().nextInt(5)+1;
+          });
+        },
+        child: Image(
+          image: AssetImage('images/ball$number.png'),
+        ),
+      )
+    );
+  }
+}
+
+
+
